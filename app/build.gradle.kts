7dc80cd59fb58be9456232e01e7a46cc39fd742a
jmccoy600 <jake@mccoy.engineer>
2026-03-23 19:12:46 -0500

    update versionName

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.androidcrypt"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
