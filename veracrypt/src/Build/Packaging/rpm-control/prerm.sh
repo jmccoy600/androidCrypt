9cdccaf72c39593f8406d518a30b68116c94fd40
jmccoy600 <jake@mccoy.engineer>
2026-03-23 19:37:19 -0500

    remove veracrypt source folder

--- veracrypt/src/Build/Packaging/rpm-control/prerm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-V="$(mount | grep veracrypt_aux_mnt)"
-if [ ! -z "$V" ]
-then
-	echo "Error: All VeraCrypt volumes must be dismounted first." >&2
-	exit 1
-else
-	exit 0
-fi 
--- veracrypt/src/Main/Forms/international.h
+++
@@ -1,6 +0,0 @@
-#ifndef TC_WINDOWS
-#include "Main/LanguageStrings.h"
-#undef _
-#define _(key) LangString[key]
-#endif
-
