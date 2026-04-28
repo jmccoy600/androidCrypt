9c3f4be33ab9409603fc78081d995adb46988903
jmccoy600 <jake@mccoy.engineer>
2026-04-27 21:46:08 -0500

    bump version to 14

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.androidcrypt"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.0.13"
+        versionCode = 14
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
